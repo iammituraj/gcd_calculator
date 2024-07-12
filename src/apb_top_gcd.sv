0c1f16bd08b327c3bc72950947e698dc2d52d11b
Mitu Raj <51367682+iammituraj@users.noreply.github.com>
2024-07-13 01:35:19 +0530

    Update apb_top_gcd.sv
    Added penable to qualifying signals for read operation

--- src/apb_top_gcd.sv
+++ src/apb_top_gcd.sv
@@ -127,8 +127,8 @@ always @(posedge clk) begin
          
          // Read Access State : waits for penable signal and reads addressed-register
          R_ACCESS : begin                   
-               // psel and pwrite expected to be stable for successful read               
-               if (i_psel && !i_pwrite) begin
+               // psel and pwrite expected to be stable and penable to be asserted for successful read               
+            if (i_psel && !i_pwrite && i_penable) begin
                   // Address decoding - two LSbs masked because 32-bit byte-addressable
                   case (i_paddr [ADDR_W-1 : 2])
                      0       : prdata_rg <= apb_reg [0] ;
